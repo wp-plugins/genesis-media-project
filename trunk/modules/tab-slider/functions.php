dc733fd79d02566cd26b0d935e893cfa8e94cc31
Nick_theGeek <Nick_theGeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-07 04:04:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/genesis-media-project/tags/0.9.0.1@515700 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/modules/tab-slider/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-add_image_size( 'tab-large', 530, 270, true );
-add_image_size( 'tab-thumbnails', 150, 100, true );
--- trunk/modules/video/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $gmp_wide;
-
-$content_width = isset( $content_width ) ? $content_width : 600;
-$width         = isset( $gmp_wide )      ? $gmp_wide      : $content_width;
-
-add_image_size( 'video', $width, ceil( $width*.5625 ), true );
